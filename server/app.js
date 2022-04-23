139138fea3e195fe4a27b44bcf0ba84b2af527a6
Chun-Lin <wulin40063@gmail.com>
2022-04-23 15:28:43 +1000

    feat: 🎸 add all capsules api endpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const dbPool = require('./db');
-const express =require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', async (req, res) => {
     const rows = await dbPool.query('SELECT * FROM spaceData');
@@ -15,5 +18,19 @@ app.get('/', async (req, res) => {
     });
 });
 
-app.listen('4000');
+app.get('/all-capsules', async (req, res) => {
+  axios
+    .get('https://api.spacexdata.com/v4/capsules')
+    .then((response) => {
+      console.log(`statusCode: ${response.status}`);
+      console.log(response);
+      res.status(response.status);
+      res.send({ data: response.data });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+app.listen('4001');
 console.log(`Listening on port: 4000, wait for the development server to be up...`);
