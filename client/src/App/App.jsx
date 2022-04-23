c9edd8b94c576c46c49280d91343a864edb503d1
Chun-Lin <wulin40063@gmail.com>
2022-04-23 20:01:39 +1000

    style: 💄 layout of the project

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,20 +1,48 @@
 import { hot } from 'react-hot-loader/root';
-import React from 'react';
-import GlobalStyle from '../theme';
-import { Application } from './styles';
+import React, { useState } from 'react';
 import { ReactComponent as Rocket } from '../assets/rocket.svg';
+import { Button, Flex, Box, Stack, Input } from '@chakra-ui/react';
 
-const App = () => (
-  <>
-    <Application>
-      <Rocket />
-      <span>
-        "Space isn't remote at all. It's only an hour's drive away, if your car could go straight
-        upwards."
-      </span>
-    </Application>
-    <GlobalStyle />
-  </>
-);
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <>
+      <Flex w="100vw" h="100vh" justify="center" align="center">
+        <Flex
+          direction={['row', 'row', 'column']}
+          w={['100%', '100%', '50%']}
+          h={['100%', '100%', '50%']}
+          p="3"
+          border={['none', 'none', '1px solid black']}
+        >
+          <Box flex="2">output</Box>
+          <Flex flex="1" direction={['column', 'column', 'row']}>
+            <Flex flex="1" justify="center" align="center">
+              <Button colorScheme="teal" size="lg">
+                Capsules
+              </Button>
+            </Flex>
+            <Flex flex="1" justify="center" align="center" bgColor="blue">
+              <Rocket />
+            </Flex>
+            <Flex flex="1" direction="row" justify="center" align="center">
+              <Stack spacing={3}>
+                <input
+                  placeholder="Input Pad ID"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <Button colorScheme="teal" size="lg">
+                  Landing Pad
+                </Button>
+              </Stack>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
 
 export default hot(App);
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
 import store from './redux';
 import App from './App';
 
 const render = (Component) => {
   ReactDOM.render(
     <Provider store={store}>
-      <Component />
+      <ChakraProvider>
+        <Component />
+      </ChakraProvider>
     </Provider>,
     document.getElementById('cape-canaveral')
   );
