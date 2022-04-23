2e1d6a20bc791fff725544bfdac2ef6616172bd1
Chun-Lin <wulin40063@gmail.com>
2022-04-23 20:36:43 +1000

    feat: 🎸 add a component to display prettify JSON output

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -2,10 +2,40 @@ import { hot } from 'react-hot-loader/root';
 import React, { useState } from 'react';
 import { ReactComponent as Rocket } from '../assets/rocket.svg';
 import { Button, Flex, Box, Stack, Input } from '@chakra-ui/react';
+import PrettifyJsonOutput from '../components/PrettifyJsonOutput';
 
 const App = () => {
   const [inputValue, setInputValue] = useState('');
 
+  const fakeJson = {
+    _id: '{{objectId()}}',
+    index: '{{index()}}',
+    guid: '{{guid()}}',
+    isActive: '{{bool()}}',
+    balance: '{{floating(1000, 4000, 2, "$0,0.00")}}',
+    picture: 'http://placehold.it/32x32',
+    age: '{{integer(20, 40)}}',
+    eyeColor: '{{random("blue", "brown", "green")}}',
+    name: '{{firstName()}} {{surname()}}',
+    gender: '{{gender()}}',
+    company: '{{company().toUpperCase()}}',
+    email: '{{email()}}',
+    phone: '+1 {{phone()}}',
+    address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}',
+    about: '{{lorem(1, "paragraphs")}}',
+    registered: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-ddThh:mm:ss Z")}}',
+    latitude: '{{floating(-90.000001, 90)}}',
+    longitude: '{{floating(-180.000001, 180)}}',
+    tags: ['{{repeat(7)}}', '{{lorem(1, "words")}}'],
+    friends: [
+      '{{repeat(3)}}',
+      {
+        id: '{{index()}}',
+        name: '{{firstName()}} {{surname()}}'
+      }
+    ]
+  };
+  
   return (
     <>
       <Flex w="100vw" h="100vh" justify="center" align="center">
@@ -16,7 +46,7 @@ const App = () => {
           p="3"
           border={['none', 'none', '1px solid black']}
         >
-          <Box flex="2">output</Box>
+          <PrettifyJsonOutput data={fakeJson} />
           <Flex flex="1" direction={['column', 'column', 'row']}>
             <Flex flex="1" justify="center" align="center" p="2">
               <Button colorScheme="teal" size="lg">
---
+++ client/src/components/PrettifyJsonOutput.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledPre = styled.pre`
+  flex: 2;
+  display: block;
+  padding: 10px;
+  overflow: scroll;
+  color: white;
+  background-color: black;
+  white-space: break-spaces;
+`;
+
+const PrettifyJsonOutput = ({data}) => {
+  return <StyledPre>{JSON.stringify(data, null, 2)}</StyledPre>;
+};
+
+export default PrettifyJsonOutput;
