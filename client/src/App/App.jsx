0c2bedea4777608461efa012ae0cf806b82afaa2
Chun-Lin <wulin40063@gmail.com>
2022-04-24 20:45:22 +1000

    refactor: 💡 remove unused fragment

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -16,41 +16,39 @@ const App = () => {
   };
 
   return (
-    <>
-      <Flex w="100vw" h="100vh" justify="center" align="center">
-        <Flex
-          direction={['row', 'row', 'column']}
-          gap="2"
-          w={['100%', '100%', '50%']}
-          h={['100%', '100%', '50%']}
-          p="3"
-          borderRadius="md"
-          bgColor="gray.50"
-          border="1px solid skyblue"
-        >
-          <PrettifyJsonOutput data={outputData?.data} />
-          <Flex flex="1" direction={['column', 'column', 'row']}>
-            <Flex flex="1" justify="center" align="center" p="2">
-              <Button
-                colorScheme="teal"
-                size="lg"
-                w="100%"
-                onClick={capsuleBtnClickHandler}
-                isLoading={isLoading?.capsules}
-              >
-                Capsules
-              </Button>
-            </Flex>
-            <Flex flex="1" justify="center" align="center" bgColor="teal.500">
-              <Rocket />
-            </Flex>
-            <Flex flex="1" direction="row" justify="center" align="center" p="2">
-              <LandingPadSearch />
-            </Flex>
+    <Flex w="100vw" h="100vh" justify="center" align="center">
+      <Flex
+        direction={['row', 'row', 'column']}
+        gap="2"
+        w={['100%', '100%', '50%']}
+        h={['100%', '100%', '50%']}
+        p="3"
+        borderRadius="md"
+        bgColor="gray.50"
+        border="1px solid skyblue"
+      >
+        <PrettifyJsonOutput data={outputData?.data} />
+        <Flex flex="1" direction={['column', 'column', 'row']}>
+          <Flex flex="1" justify="center" align="center" p="2">
+            <Button
+              colorScheme="teal"
+              size="lg"
+              w="100%"
+              onClick={capsuleBtnClickHandler}
+              isLoading={isLoading?.capsules}
+            >
+              Capsules
+            </Button>
+          </Flex>
+          <Flex flex="1" justify="center" align="center" bgColor="teal.500">
+            <Rocket />
+          </Flex>
+          <Flex flex="1" direction="row" justify="center" align="center" p="2">
+            <LandingPadSearch />
           </Flex>
         </Flex>
       </Flex>
-    </>
+    </Flex>
   );
 };
 
